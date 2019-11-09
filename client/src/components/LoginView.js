363a707991386d18204eb56f3b0294f551458f87
HungNP89 <loliology9029@gmail.com>
2019-11-09 17:52:12 +0200

    fix

--- client/src/components/LoginView.js
+++ client/src/components/LoginView.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
-
+import styles from './Auth.module.css';
 class LoginView extends Component {
 
     render() {
         return (
-        <div className="FormCenter">
-            <form  className="FormFields" onSubmit={this.handleSubmit}>
+        <div className={styles.FormCenter}>
+            <form  className={styles.FormFields} onSubmit={this.handleSubmit}>
             <h1>Sign In</h1>
-            <div className="FormField">
-                <label className="FormLabel" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormInput" placeholder="Enter your Email" name="email" />
+            <div className={styles.FormField}>
+                <label className={styles.FormLabel} htmlFor="email">E-Mail Address</label>
+                <input type="email" id="email" className={styles.FormInput} placeholder="Enter your Email" name="email" />
               </div>
 
-              <div className="FormField">
-                <label className="FormLabel" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormInput" placeholder="Enter your password" name="password"  />
+              <div className={styles.FormField}>
+                <label className={styles.FormLabel} htmlFor="password">Password</label>
+                <input type="password" id="password" className={styles.FormInput} placeholder="Enter your password" name="password"  />
               </div>
 
-              <div className="FormField">
-                  <button className="FormButton mr-20" type="submit">Sign In</button> 
+              <div className={styles.FormField}>
+                  <button className={styles.FormButton} type="submit">Sign In</button> 
               </div>
             </form>
           </div>
--- client/src/components/RegisterView.js
+++ client/src/components/RegisterView.js
@@ -1,40 +1,40 @@
 import React , { Component} from 'react'
-
+import styles from './Auth.module.css';
 
 class RegisterView extends Component {
 
    render() {
        return (
-           <div className="FormCenter">
-               <form onSubmit={this.handleSubmit} className="FormFields">
+           <div className={styles.FormCenter}>
+               <form onSubmit={this.handleSubmit} className={styles.FormFields}>
                    <h1>Sign Up</h1>
-                   <div className="FormField">
-                       <label className="FormLabel" htmlFor="name">Name</label>
-                       <input type="text" id="name" className="FormInput" placeholder="Enter your name" name="name" />
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="name">Name</label>
+                       <input type="text" id="name" className={styles.FormInput} placeholder="Enter your name" name="name" />
                    </div>
-                   <div className="FormField">
-                       <label className="FormLabel" htmlFor="doB">DoB</label>
-                       <input type="text" id="doB" className="FormInput" placeholder="Enter your DoB" name="doB" />
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="doB">DoB</label>
+                       <input type="text" id="doB" className={styles.FormInput} placeholder="Enter your DoB" name="doB" />
                    </div>
-                   <div className="FormField">
-                       <label className="FormLabel" htmlFor="email">Email</label>
-                       <input type="email" id="email" className="FormInput" placeholder="Enter your Email" name="email"/>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="email">Email</label>
+                       <input type="email" id="email" className={styles.FormInput} placeholder="Enter your Email" name="email"/>
                    </div>
-                   <div className="FormField">
-                       <label className="FormLabel" htmlFor="username">UserName</label>
-                       <input type="text" id="username" className="FormInput" placeholder="Enter your Username" name="username"/>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="username">UserName</label>
+                       <input type="text" id="username" className={styles.FormInput} placeholder="Enter your Username" name="username"/>
                    </div>
-                   <div className="FormField">
-                       <label className="FormLabel" htmlFor="password">Password</label>
-                       <input type="password" id="password" className="FormInput" placeholder="Enter your Password" name="password" />
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="password">Password</label>
+                       <input type="password" id="password" className={styles.FormInput} placeholder="Enter your Password" name="password" />
                    </div>
-                   <div className="FormField">
-                       <label className="FormLabel" htmlFor="passwordConfirm">Confirm Password</label>
-                       <input type="password" id="passwordConfirm" className="FormInput" placeholder="Confirm your password" name="passwordConfirm" />
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="passwordConfirm">Confirm Password</label>
+                       <input type="password" id="passwordConfirm" className={styles.FormInput} placeholder="Confirm your password" name="passwordConfirm" />
                    </div>
 
-                   <div className="FormField">
-                       <button className="FormButton mr-20">Sign Up</button>
+                   <div className={styles.FormField}>
+                       <button className={styles.FormButton}>Sign Up</button>
                    </div>
                </form>
            </div>
