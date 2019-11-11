16849c45d13637236d5f17aecc540237380e32b8
Lam Pham <t8phho00@students.oamk.fi>
2019-11-11 09:48:51 +0200

    commit in register

--- client/src/components/RegisterView.js
+++ client/src/components/RegisterView.js
@@ -64,29 +64,38 @@ register = (event) =>
     return(
       <div>
       <h1>Register</h1>
-      <form onSubmit={this.register}>
-      <div>
-        Name <input type="text" name="name" />
-      </div>
-      <div>
-        Date of birth <input type="date" name="doB" />
-      </div>
-      <div>
-        email <input type="text" name="email" />
-      </div>
-        <div>
-          Username <input type="text" name="username" />
-        </div>
-        <div>
-          Password <input type="password" name="password" />
-        </div>
-        <div>
-          Confirm Password <input type="password" name="passwordConfirm" />
-        </div>
-        <div>
-          <button type="submit">Register</button>
-        </div>
-      </form>
+      <div className={styles.FormCenter}>
+               <form onSubmit={this.register} className={styles.FormFields}>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="name">Name</label>
+                       <input type="text" id="name" className={styles.FormInput} placeholder="Enter your name" name="name" />
+                   </div>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="doB">DoB</label>
+                       <input type="text" id="doB" className={styles.FormInput} placeholder="Enter your DoB" name="doB" />
+                   </div>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="email">Email</label>
+                       <input type="email" id="email" className={styles.FormInput} placeholder="Enter your Email" name="email"/>
+                   </div>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="username">UserName</label>
+                       <input type="text" id="username" className={styles.FormInput} placeholder="Enter your Username" name="username"/>
+                   </div>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="password">Password</label>
+                       <input type="password" id="password" className={styles.FormInput} placeholder="Enter your Password" name="password" />
+                   </div>
+                   <div className={styles.FormField}>
+                       <label className={styles.FormLabel} htmlFor="passwordConfirm">Confirm Password</label>
+                       <input type="password" id="passwordConfirm" className={styles.FormInput} placeholder="Confirm your password" name="passwordConfirm" />
+                   </div>
+
+                   <div className={styles.FormField}>
+                       <button className={styles.FormButton}>Sign Up</button>
+                   </div>
+               </form>
+           </div>
       </div>
     )
   }
