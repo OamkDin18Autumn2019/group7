486ad4529e51fa91668acf8e6438339ee811c809
dinhtai0401 <dinhtai040197@gmail.com>
2019-11-11 06:45:19 -0500

    new

--- server/database.js
+++ server/database.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
   host: 'localhost',
-  user: 'dinhtai41',
-  password: '!b15fc4be24263482874bf8bdb20e7a62D',
-  database: 'demo'
+  user: 'netpass',
+  password: 'netpass',
+  database: 'blog'
 });
 module.exports = connection;
