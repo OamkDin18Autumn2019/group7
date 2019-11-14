8ab63c861fb3ca0f154f3c908860344f22543a79
dinhtai0401 <dinhtai040197@gmail.com>
2019-11-14 05:38:48 -0500

    new

--- server/database.js
+++ server/database.js
@@ -8,9 +8,9 @@ try {
   pool  = mysql.createPool({
     connectionLimit : 10,
     host            : 'localhost',
-    user            : 'dinhtai41',
-    password        : '!b15fc4be24263482874bf8bdb20e7a62D',
-    database        : 'demo'
+    user            : 'netuser',
+    password        : 'netpass',
+    database        : 'blog'
   });
 
 } catch (error) {
