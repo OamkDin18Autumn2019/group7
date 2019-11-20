e924b41977b362d90599497ab444252150b01185
t8ngph00 <t8ngph00@students.oamk.fi>
2019-11-20 13:26:43 +0200

    'new'

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="mainmenu" >
+  <div className="topnav">
+    <div className="home">
+      <a className="active" href="#home">Home</a>
+    </div>
+      <a >About</a>
+      <div className="container-1">
+
+
+        <input type="search" id="search" placeholder="Search..." />
+
+      </div>
+
+      <div className="login-container">
+          
+          <button type="submit">Log in</button>
+          <button type="submit">Register</button>     
+          </div>
+      </div>
+    </div>
+    
+              <div class="topnav2">
+          <ul>
+                  <img class="logo" src={"2019-11-14.png"} alt=''/>
+          <li><a href="#">Rescipes</a>
+           <ul>
+            <li><a href="">Diet Menu</a></li>
+            <li><a href="">Weight Menu</a></li>
+            <li><a href="">Everday Meals</a></li>
+           </ul>
+          </li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">Contact</a>
+              <ul>
+               <li><a href="">Call</a></li>
+               <li><a href="">Emails</a></li>
+              </ul>
+             </li>
+          
+         </ul>
+        </div>
+        <div class="page">
+                        <div class="page__demo">
+                          <div class="page__container">
+                            <div class="photobox photobox_type1">
+                              <div class="photobox__previewbox">
+                                <img src="am-thuc.jpg" class="photobox__preview" alt="Preview" />
+                                <span class="photobox__label">My favorites </span>
+                              </div>
+                            </div>
+                            <div class="photobox photobox_type2">
+                              <div class="photobox__previewbox">
+                                <img src="bun-bo-hue.jpg" class="photobox__preview" alt="Preview" />
+                                <span class="photobox__label">Beef Noodles</span>
+                              </div>
+                            </div>
+                            <div class="photobox photobox_type3">
+                              <div class="photobox__previewbox">
+                                <img src="banh-mi.jpg" class="photobox__preview" alt="Preview" />
+                                <span class="photobox__label">Banh Mi</span>
+                              </div>
+                            </div>
+                            <div class="photobox photobox_type4">
+                              <div class="photobox__previewbox">
+                                <img src="bun-thit-nuong.jpg" class="photobox__preview" alt="Preview" />
+                                <span class="photobox__label">Bun Thit Nuong</span>
+                              </div>
+                            </div>
+                            <div class="photobox photobox_type5">
+                              <div class="photobox__previewbox">
+                                <img src="che.jpg" class="photobox__preview" alt="Preview" />
+                                <span class="photobox__label">Sweet</span>
+                              </div>
+                            </div>
+                            <div class="photobox photobox_type6">
+                              <div class="photobox__previewbox">
+                                <img src="ocsaigon.jpg" class="photobox__preview" alt="Preview" />
+                                <span class="photobox__label">Sea food</span>
+                              </div>
+                            </div>
+                            <div class="photobox photobox_type7">
+                              <div class="photobox__previewbox">
+                                <img src="pho2.webp" class="photobox__preview" alt="Preview" />
+                                <span class="photobox__label">Pho</span>
+                              </div>
+                            </div>
+                            
+                          </div>  
+                        </div>
+                      </div>     
     </div>
   );
 }
