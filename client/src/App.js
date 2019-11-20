18255d1d88ff3c68d86e4a108aac0c5ae86e4324
HungNP89 <loliology9029@gmail.com>
2019-11-20 10:03:07 +0200

    fix switcher

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter as Router, Route,  NavLink } from 'react-router-dom';
 import './App.css';
 import RegisterView from './components/RegisterView.js';
-
+import LoginView from './components/LoginView.js';
 class App extends React.Component
 {
   constructor(props){
@@ -14,6 +14,7 @@ class App extends React.Component
     return(
     <Router>
          <Route path="/register" exact render={ routeProps => <RegisterView {...routeProps}/> }/>
+         <Route path="/login" exact render={ routeProps => <LoginView {...routeProps}/>}/>
     </Router>
       
   )
---
+++ client/src/components/LoginView.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Auth from './Auth';
+import styles from './Auth.module.css';
+import { BrowserRouter as Router, Route,  NavLink } from 'react-router-dom';
+export default function LoginView(props) {
+
+  function login(event)
+  {
+    event.preventDefault();
+    Auth.authenticate(event.target['username'].value, event.target['password'].value)
+      .then(result =>
+        {
+          props.loginSuccess();
+          props.history.push(props.redirectPathOnSuccess);
+        })
+      .catch(() => {
+        props.loginFail();
+      })
+
+  }
+
+  return (
+    <div className="App">
+    <div className="AppAside"></div>
+    <div className="AppForm">
+    <div className="PageSwitcher">
+        <NavLink to="/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+        <NavLink exact to="/register" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+    </div>
+    <h1>Sign In</h1>
+    <div className={styles.FormCenter}>
+        <form  className={styles.FormFields} onSubmit={login}>
+        <div className={styles.FormField}>
+            <label className={styles.FormLabel} htmlFor="username">Username</label>
+            <input type="text" id="username" className={styles.FormInput} placeholder="Enter your Username" name="username" />
+          </div>
+
+          <div className={styles.FormField}>
+            <label className={styles.FormLabel} htmlFor="password">Password</label>
+            <input type="password" id="password" className={styles.FormInput} placeholder="Enter your password" name="password"  />
+          </div>
+
+          <div className={styles.FormField}>
+              <button className={styles.FormButton} type="submit">Sign In</button> 
+          </div>
+        </form>
+        </div>
+        </div>
+      </div>
+    )
+}
