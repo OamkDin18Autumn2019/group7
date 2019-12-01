a2612ef5bba2811afe119db898e88fea7efe1348
Lam Pham <t8phho00@students.oamk.fi>
2019-12-01 06:44:43 +0200

    add files

--- client/src/components/AddComment.js
+++ client/src/components/AddComment.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import styles from './AddComment.module.css';
-  export default function AddComment(props) {
-  function  update(event)
-   {
-    props.onHandleChange(event.target.value);
-  }
-          return (
-              <div>
-              <h2>Add Comment</h2>
+export default function AddComment(props) {
+    function update(event) {
+        props.onHandleChange(event.target.value);
+    }
+    return (
+        <div>
+            <h2>Add Comment</h2>
 
-                <textarea className={styles.input} onChange={update} type='text'  placeholder="Comment" name='comment' id='comment' value={props.input}></textarea>
-                <button className={styles.button} onClick={props.submit}>Comments</button>
+            <textarea className={styles.input} onChange={update} type='text' placeholder="Comment" name='comment' id='comment' value={props.input}></textarea>
+            <button className={styles.button} onClick={props.submit}>Comments</button>
 
-              </div>
+        </div>
 
-          );
-  }
+    );
+}
