add5d6d17ec323f716ed57a8bb11d34e8b89151d
HungNP89 <loliology9029@gmail.com>
2019-12-04 13:41:28 +0200

    fix

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -4,12 +4,9 @@ import styles from './Comments.module.css';
 export default function Comments(props) {
 
     return (
-        /*<div>
-            <h2>Comments</h2>
-            {props.comments.map(i => <div className={styles.comment} key={i.idcomments}>{i.username} : {i.comment}</div>)}
-        </div>*/
         <div className={styles.comment}>
             <h2>Comments</h2>
+            {props.comments.map(i => <div className={styles.comment} key={i.idcomments}>{i.username} : {i.comment}</div>)}
         </div>
 
     );
