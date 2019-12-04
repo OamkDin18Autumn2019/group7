498490e1d1f6ad3f9429a44a264a3bbed76697ff
Lam Pham <t8phho00@students.oamk.fi>
2019-12-04 12:03:42 +0200

    fix delete post component

--- client/src/components/UserPost.js
+++ client/src/components/UserPost.js
@@ -9,54 +9,58 @@ export default class PostList extends Component {
             post: []
         };
     }
-    
+
     componentDidMount = () => {
-        if(this.props.userInfo != null)
-        {
-        axios.get(
+        if (this.props.userInfo != null) {
+            axios.get(
                 "http://localhost:4000/post/user/" +
                 this.props.userInfo.id
             )
-            .then(res => {
-                this.setState({ post: res.data });
-                console.log(this.state.post);
-            })
-            .catch(error => console.log(error));
+                .then(res => {
+                    this.setState({ post: res.data });
+                    console.log(this.state.post);
+                })
+                .catch(error => console.log(error));
         }
     }
-    alo = (id)=>{
-       axios.delete( "http://localhost:4000/post/" + id
-       ).then(function (response) {
-        console.log(response);
+    deletePost = (id) => {
+        axios.delete("http://localhost:4000/post/" + id
+        ).then(function (response) {
+            console.log(response);
         })
-      .catch(function (error) {
-        console.log(error);
-      }); 
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     render() {
-        if (this.props.userInfo === null)
-        {
-            return(<React.Fragment><Redirect to='/login' /></React.Fragment>)
+        if (this.props.userInfo === null) {
+            return (<React.Fragment><Redirect to='/login' /></React.Fragment>)
         }
-        else{
-        return(
-            <div>
-                <table className={styles.border}>
-                <tr className={styles.border}>
-                    <td className={styles.border}>id</td>
-                    <td className={styles.border}>Post Name</td>
-                    <td className={styles.border}>Date</td>
-                    <td className={styles.border}>Delete</td>
-                </tr>
-                {this.state.post.map(i=>
-                <tr className={styles.border}>
-                    <td className={styles.border}>{i.idposts}</td>
-                    <td className={styles.border}>{i.name}</td>
-                    <td className={styles.border}>{i.date}</td>
-                    <td className={styles.border}><button onClick={this.alo.bind(this,i.idposts)}>Delete</button></td>
-                </tr>)}
-                </table>
-            </div>
-        )}
+        else {
+            if (this.props.post === null) {
+                return <h2>You have no post</h2>
+            }
+            else {
+                return (
+                    <div>
+                        <table className={styles.border}>
+                            <tr className={styles.border}>
+                                <td className={styles.border}>id</td>
+                                <td className={styles.border}>Post Name</td>
+                                <td className={styles.border}>Date</td>
+                                <td className={styles.border}>Delete</td>
+                            </tr>
+                            {this.state.post.map(i =>
+                                <tr className={styles.border}>
+                                    <td className={styles.border}>{i.idposts}</td>
+                                    <td className={styles.border}>{i.name}</td>
+                                    <td className={styles.border}>{i.date}</td>
+                                    <td className={styles.border}><button onClick={this.deletePost.bind(this, i.idposts)}>Delete</button></td>
+                                </tr>)}
+                        </table>
+                    </div>
+                )
+            }
+        }
     }
 }
