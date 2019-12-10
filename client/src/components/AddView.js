3495762834affb573cd4b46e1f105607526d0b66
HungNP89 <loliology9029@gmail.com>
2019-12-10 21:35:46 +0200

    UI fix v3

--- client/src/components/AddView.js
+++ client/src/components/AddView.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import styles from './AddView.module.css';
-
+import { Redirect, Route } from "react-router-dom";
 const endpoint = 'http://localhost:5000/upload/'
 
 class AddView extends React.Component {
@@ -47,49 +47,27 @@ class AddView extends React.Component {
     const mystyle = {
       overflow: "hidden", display:"inline-block"  , resize: "none", height: "160px" 
     };
-    return (
- /*<div className={styles.wrapper}>
-     <form className={styles.paper} method="get" action=""  onSubmit={this.handleSubmit}>
-     <div >
-         <label>Name</label>
-         <input type="text" id="name" name="name" />
-       </div>
-
-       <div >
-         <label>Ingredients</label>
-         <textarea rows="10" type="text" id="ingredients" name="ingredients"  />
-       </div>
-
-       <div>
-         <label>Recipe</label>
-         <textarea rows="10" type="text" id="recipe"  name="recipe"  />
-       </div>
-
-       <div>
-         <label>Image</label>
-         <input type="file" id="image" onChange={this.handleselectedFile} name="image"  />
-       </div>
-
-
-
-       <div>
-           <button type="submit" onClick={this.handleUpload}>Submit</button>
-       </div>
-     </form>
-</div>*/
-      <div className={styles.View}>
-      <div className={styles.wrapper}>
-        <form className={styles.paper} method="get" action="" onSubmit={this.handleSubmit}>
-          <div className={styles.margin} > <input placeholder="Name" className={styles.name} type="text" name="name"></input></div>
-          <textarea placeholder="Enter Ingredients" className={styles.text} name="ingredients" rows="4" style={mystyle}></textarea>
-          <textarea placeholder="Enter Recipe" className={styles.text} name="recipe" rows="4" style={mystyle}></textarea>
-          <div className={styles.margin} placeholder="Image"> <input className={styles.text} type="file" id="image" onChange={this.handleselectedFile} name="image"></input></div>
-          <div><button className={styles.button} type="submit" onClick={this.handleUpload}>Submit</button></div>
-        </form>
-      </div>
-      </div>
-    )
+    if(this.props.userInfo == null){
+      alert("Must be login first")
+         return(<Redirect to='/' />)
+    } else {
+        {
+            console.log(this.state.imageURL)
+            return (
+              <div className={styles.View}>
+              <div className={styles.wrapper}>
+                <form className={styles.paper} method="get" action="" onSubmit={this.handleSubmit}>
+                  <div className={styles.margin} > <input placeholder="Name" className={styles.name} type="text" name="name"></input></div>
+                  <textarea placeholder="Enter Ingredients" className={styles.text} name="ingredients" rows="4" style={mystyle}></textarea>
+                  <textarea placeholder="Enter Recipe" className={styles.text} name="recipe" rows="4" style={mystyle}></textarea>
+                  <div className={styles.margin} placeholder="Image"> <input className={styles.text} type="file" id="image" ref={ref => {this.uploadInput = ref;}} name="image"></input></div>
+                  <div><button className={styles.button} type="submit" >Submit</button></div>
+                </form>
+              </div>
+              </div>
+            )
+        }
+    }
   }
 }
-
 export default AddView
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,35 +2,69 @@ import React, {Component} from 'react';
 import styles from './App.module.css';
 import { BrowserRouter as BrowserRouter,  Router, Route, Link, Redirect  } from 'react-router-dom';
 
-export default function Header(props){
-  return(
-    <div>
-      <div className={styles.mainmenu} >
-        <div className={styles.topnav}>
-          <div className={styles.home}>
-            <Link to="/" className="active">Home</Link>
+export default class Header extends React.Component {
+  render(){
+    if(this.props.userInfo == null){
+      return(
+        <header>
+          <div>
+            <div className={styles.mainmenu}>
+              <div className={styles.topnav}>
+                <div className={styles.home}>
+                  <Link to="/" className="active">Home</Link>
+                </div>
+                  <a >About</a>
+                  <div className={styles.container}>
+                    <input type="search" values={this.props.search} onChange={this.props.updateSearch} className={styles.search} placeholder="Search..." />
+                  </div>
+                <div className={styles.login}>
+                  <Link to='/login'>Login</Link>
+                  <Link to='/logout'>Logout</Link>
+                </div>
+            </div>
           </div>
-          <a >About</a>
-          <div className={styles.container}>
-            <input type="search" values={props.search} onChange={props.updateSearch} className={styles.search} placeholder="Search..." />
+          <div className={styles.topnav2}>
+            <ul>
+              <img className={styles.logo} src={require('../header.png')} alt=''/>
+              <li><a href="#">Recipes</a></li>
+              <li><Link to='/list'>Item</Link></li>
+              <li><Link to='/post'>Post</Link></li>
+            </ul>
           </div>
-          <div className={styles.login}>
-            <Link to='/login'>Login</Link>
-            <Link to='/logout'>Logout</Link>
           </div>
-        </div>
-      </div>
-
-      <div className={styles.topnav2}>
-        <ul>
-          <img className={styles.logo} src={require('../header.png')} alt=''/>
-          <li><a href="#">Recipes</a>
-          </li>
-          <li><Link to='/list'>Item</Link></li>
-          <li><Link to='/post'>Post</Link></li>
-        </ul>
-      </div>
-    </div>
- )
+        </header>
+      )
+    }else{
+      return(
+        <header>
+          <div>
+            <div className={styles.mainmenu} >
+              <div className={styles.topnav}>
+                <div className={styles.home}>
+                  <Link to="/" className="active">Home</Link>
+                </div>
+                  <a >About</a>
+                  <div className={styles.container}>
+                    <input type="search" values={this.props.search} onChange={this.props.updateSearch} className={styles.search} placeholder="Search..." />
+                  </div>
+                <div className={styles.login}>
+                    {this.props.userInfo.name}
+                    <button onClick={this.props.onLogOut}>Logout</button>
+                </div>
+              </div>
+            </div>
+            <div className={styles.topnav2}>
+              <ul>
+                <img className="logo" src={require('../header.png')} alt=''/>
+                <li><a href="#">Recipe</a></li>
+                <li><Link to='/list'>Item</Link></li>
+                <li><Link to='/post'>Post</Link></li>
+              </ul>
+            </div>
+          </div>
+        </header>
+      )
+    }
+  }
 
 }
