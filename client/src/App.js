82e636b3749ccdfaa8c91f8015f3e52564f9708b
Lam Pham <t8phho00@students.oamk.fi>
2019-12-16 11:10:29 +0200

    fix delete function and add login alert

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,7 @@ class App extends React.Component
     this.setState({ isAuthenticated: false });
     console.log("Login failed");
     console.log(this.state.userInfo);
+    alert('Wrong username or password');
   }
   updateSearch = (event) =>{
     this.setState({search: event.target.value.substr(0,20)});
